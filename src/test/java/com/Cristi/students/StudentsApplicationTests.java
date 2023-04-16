3f921a27924704ec7c3b069ae51693db300b89e6
Cristian-Dragusin <cristian.dragusin@microfocus.com>
2023-04-16 20:30:15 +0300

    Original Spring Application

---
+++ src/test/java/com/Cristi/students/StudentsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Cristi.students;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
